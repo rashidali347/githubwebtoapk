3ca32fb8a0e2df4d97fb4fa18ff48a9fe8b5b142
rashidali347 <rashidali47083490@gmail.com>
2026-05-07 18:34:37 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -8,9 +7,8 @@ plugins {
 }
 
 android {
-    // پلے اسٹور کی ضرورت کے مطابق API لیول 34
     compileSdk = 34
-    namespace = "com.trustrium.mining" // آپ کی کمپنی کا یونیک پیکیج نیم
+    namespace = "com.trustrium.mining"
 
     defaultConfig {
         applicationId = "com.trustrium.mining"
@@ -20,43 +18,18 @@ android {
         versionName = "1.0.0"
 
         multiDexEnabled = true
-        vectorDrawables.useSupportLibrary = true
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        // یہاں Trustrium کی تفصیلات سیٹ کر دی گئی ہیں
         resValue("string", "url_web", "https://app.trustrium.com")
         resValue("string", "app_name", "Trustrium Mining")
-        
-        // یہ لائن TWA (Trusted Web Activity) کے لیے ضروری ہے
-        manifestPlaceholders["assetStatements"] = """
-            [{
-              "relation": ["delegate_permission/common.handle_all_urls"],
-              "target": {
-                "namespace": "android_app",
-                "package_name": "com.trustrium.mining",
-                "sha256_cert_fingerprints": [""] 
-              }
-            }]
-        """.trimIndent()
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true 
+            isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            
-            // عارضی طور پر ڈیبگ سائننگ استعمال کر رہے ہیں تاکہ GitHub بلڈ فیل نہ ہو
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
-        }
-        
-        debug {
-            applicationIdSuffix = ".debug"
-            isDebuggable = true
         }
     }
 
@@ -65,10 +38,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
+    // یہاں میں نے تبدیلی کی ہے تاکہ وہ ایرر ختم ہو جائے
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -79,19 +51,12 @@ android {
 }
 
 dependencies {
-    // Android Standard Libraries
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    
-    // TWA Library (Trusted Web Activity) - ویب سائٹ کو ایپ میں بدلنے کے لیے
     implementation("com.google.androidbrowserhelper:androidbrowserhelper:2.5.0")
-
-    // Background tasks (مائننگ ایپس کے لیے اکثر ضروری ہوتی ہے)
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
-
-    // Testing Libraries
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
